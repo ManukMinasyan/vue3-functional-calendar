be9774bcb636e927570fed5efbfce990ac3be2ed
Ilya <ilya.w.a.j@gmail.com>
2022-07-07 19:03:21 +0400

    start

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<template lang="html">
+  <div>
+    <h1 class="text-center mt-6 font-medium text-3xl">Welcom Vue 3 Functional Calendar</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+export default {
+  
+}
 </script>
+<style lang="">
+  
+
+</style>
